a316e898563cc10abe80d3254a51d0a5a7733ff0
yenchih123 <yenchih.andrew.tang@gmail.com>
2016-04-18 15:46:53 -0700

    Changed parameter to accept list instead of file

--- ScrabbleWordCalculator/src/ScrabbleWordTest.java
+++ ScrabbleWordCalculator/src/ScrabbleWordTest.java
@@ -1,12 +1,21 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
 /*
- * Tests Scrabble word calculator by printing out the highest scoring word
+ * Tests Scrabble word calculator by printing out the highest scoring word from a list
+ * of words from a text file
  */
 public class ScrabbleWordTest {
 	public static void main(String[] args) {
 		try {
-			System.out.println(WordCalculator.calculateBestWord(new File("words.txt")));
+			Scanner sc = new Scanner(new File("words.txt"));
+			List<String> list = new LinkedList<String>();
+			while(sc.hasNextLine()) {
+				list.add(sc.nextLine());
+			}
+			System.out.println(WordCalculator.calculateBestWord(list));
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
--- ScrabbleWordCalculator/src/WordCalculator.java
+++ ScrabbleWordCalculator/src/WordCalculator.java
@@ -1,23 +1,20 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.List;
 import java.util.Scanner;
 /*
- * Takes in a text file and returns the highest scoring Scrabble word
+ * Takes a list and returns the highest scoring Scrabble word
  */
 public class WordCalculator {
 	private static int[] arr = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8,
-	                     5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 
-	                     1, 4, 4, 8, 4, 10};
-	
-	public static String calculateBestWord(File f) throws FileNotFoundException {
+			5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 
+			1, 4, 4, 8, 4, 10};
+
+	public static String calculateBestWord(List<String> l) {
 		String maxWord = "";
 		int maxScore = 0;
-		Scanner sc = new Scanner(f);
-		String word = "";
-		int score;
-		while(sc.hasNextLine()) {
-			score = 0;
-			word = sc.nextLine();
+		int score = 0;
+		for (String word: l) {
 			for (int i = 0; i < word.length(); i++) {
 				char c = Character.toLowerCase(word.charAt(i));
 				int index = (int)c - 97;
@@ -27,6 +24,7 @@ public class WordCalculator {
 				maxScore = score;
 				maxWord = word;
 			}
+			score = 0;
 		}
 		return maxWord;
 	}
